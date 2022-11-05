bffc1cb6d53277f72b7d2819a7b923852fe0050f
Lenovo 15 <information@com1software.com>
2022-11-05 13:54:37 -0400

    update

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"runtime"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	fmt.Printf("C1D0U484 Inline Parser Utility - (c) Copyright Com1Software 1992-2022\n")
+	fmt.Printf("Repository : github.com/Com1Software/Inline-Parser-Utility\n")
+	fmt.Printf("Operating System : %s\n", runtime.GOOS)
+	exefile := "/C1D0U484/C1D0U484.EXE"
+
+	if CheckForFile(exefile) {
+		fmt.Printf("- Parser Detected")
+	}
+
+}
+
+func CheckForFile(path string) bool {
+	file, err := os.Open(path)
+	if err != nil {
+		return false
+	}
+	file.Close()
+	return true
 }
